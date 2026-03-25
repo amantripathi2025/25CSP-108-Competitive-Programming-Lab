81998868598e43f9e0af5461bee4582128b7cd85
Aman Pati Tripathi <25LBCS1236@culkomail.in>
2026-03-25 21:25:35 +0530

    Add files via upload

---
+++ program14.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define MAX_SIZE 100
+// Global stack and top variable declaration
+char stack[MAX_SIZE];
+int top = -1;
+// Function to push a character onto the stack
+void push(char data) {
+   if (top == MAX_SIZE - 1) {
+       printf("Stack Overflow!\n");
+       return;
+   }
+   top++;
+   stack[top] = data;
+}
+// Function to pop a character from the stack
+char pop() {
+   if (top == -1) {
+       printf("Stack Underflow!\n");
+       return '\0';
+   }
+   char data = stack[top];
+   top--;
+   return data;
+}
+
+void reverse_string(char *str) {
+   int len = strlen(str);
+   
+   for (int i = 0; i < len; i++) {
+       push(str[i]);
+   }
+   
+   for (int i = 0; i < len; i++) {
+       str[i] = pop();
+   }
+}
+int main() {
+   char text[MAX_SIZE];
+   printf("Input a string: ");
+   fgets(text, MAX_SIZE, stdin);
+  
+   text[strcspn(text, "\n")] = '\0';
+  
+   reverse_string(text);
+   
+   printf("Reversed string using a stack is: %s\n", text);
+   return 0;
+}
